e224da424b88d621388b2961e90b9e3f62a31455
yuempek <yuempek@yahoo.com>
2019-07-25 09:28:24 +0300

    Create randomd.c

---
+++ randomd.c
@@ -0,0 +1,18 @@
+#include <time.h>
+#include <stdlib.h>
+/*
+  Copyright © 2019 Yuempek
+  free to use!
+*/
+
+double randomd()   /* uniform distribution [0..1] */
+{
+  int i;
+  unsigned long x = 0;
+
+  srand(clock());
+  for(i = 0; i < 8; i++)
+      x = (x << 8) | (rand() & 0xFF);
+
+  return 1.0*x / (unsigned long)(-1);
+}
